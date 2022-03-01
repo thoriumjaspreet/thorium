2cb78d321bfb599537cd94b3353bcd7494a29c68
thoriumjaspreet <jaspreet98cheema@gmail.com>
2022-03-01 18:26:45 +0530

    origin/session/mongo/session1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
-//const app = express();
+const { default: mongoose} = require('mongoose');
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
